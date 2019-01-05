2cb05300eaac401b06e80e8450c9c4eb7e023ef7
Mahfoudh AROUS <aladin02dz@hotmail.fr>
2019-01-05 08:16:46 +0100

    Serve local the web app

--- functions/getusers.js
+++ functions/getusers.js
@@ -1,7 +1,36 @@
+const axios = require('axios');
+
 exports.handler = function(event, context, callback){
-    const { name } = JSON.parse(event.body);
-    callback(null, {
-        statusCode:200,
-        body: JSON.stringify({msg: 'Hello ' + name})
-    });
+    const API_URL = 'https://api.github.com/users';
+    const API_CLIENT_ID = 'xxxx';
+    const API_CLIENT_SECRET = 'yyyy';
+
+    const URL = `${API_URL}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`;
+
+    //Send User Response
+    const send = body => {
+        callback(null, {
+            statusCode: 200,
+            headers : {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 
+                'Origin, X-Requested-With, Content-Type, Accept'
+            },
+            body: JSON.stringify(body)
+        });
+    }
+
+    //Perform API Call
+    const getUsers = () => {
+        axios.get(URL)
+            .then(res => send(res.data))
+            .catch(err => send(err));
+    }
+
+    //Make sure method is GET
+    if(event.httpMethod == 'GET'){
+        //Run
+        getUsers();
+    }
+
 }
